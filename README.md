289e3a4bcdffb4d7ba532b38655c550ea2e70c67
xuzhi.mxz <xuzhi.mxz@alibaba-inc.com>
2018-08-31 15:13:14 +0800

    init: init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# iki - toollist
+> a simple solution for you build tool list
+>
+> will support three different mod
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "index.js",
+        path: __dirname + "/lib"
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
